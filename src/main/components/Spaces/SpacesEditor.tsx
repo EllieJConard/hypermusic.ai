4f010fdd0ba95228ef1693b1fdc420c67f9a5d7c
sunsetsobserver <adamlukawski@me.com>
2023-07-26 17:31:43 +0200

    GPT working 10

--- src/main/components/Spaces/SpacesEditor.tsx
+++ src/main/components/Spaces/SpacesEditor.tsx
@@ -1,7 +1,20 @@
+import styled from "@emotion/styled"
 import { observer } from "mobx-react-lite"
 import { useStores } from "../../hooks/useStores"
-import { WindowState } from "../../stores/SpacesStore"
+import { Tab as NavigationTab } from "../Navigation/Navigation"
 import Window from "../Window/Window"
+
+const ButtonPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: ${({ theme }) => theme.backgroundColor};
+  height: 3rem;
+  flex-shrink: 0;
+`
+
+const Tab = styled(NavigationTab)`
+  cursor: pointer;
+`
 
 const SpacesEditor = observer(() => {
   const { spacesStore } = useStores()
@@ -19,6 +32,9 @@ const SpacesEditor = observer(() => {
   return (
     <div>
       <h1>Spaces</h1>
+      <ButtonPanel>
+        <Tab onClick={handleAddWindow}>Add Window</Tab>
+      </ButtonPanel>
       <div
         style={{
           width: "1000px",
@@ -40,7 +56,6 @@ const SpacesEditor = observer(() => {
           </Window>
         ))}
       </div>
-      <button onClick={handleAddWindow}>Add Window</button>
     </div>
   )
 })
